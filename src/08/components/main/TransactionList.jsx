57d0e0bd0d9b7cb29c04ad51d2e3b0291914c8bb
mttcjd <cbsjhy@gmail.com>
2021-04-13 22:51:00 +0900

    0413

--- src/08/components/main/TransactionList.jsx
+++ src/08/components/main/TransactionList.jsx
@@ -12,32 +12,17 @@ import TransactionTable from './TransactionTable';
 // import TransactionPaginationContainer from '../../containers/main/TransactionPaginationContainer';
 
 class TransactionList extends PureComponent {
-  state = {
-    transactions: [
-      // {
-      //   id: 'btx_01',
-      //   name: '비트코인(BTC)',
-      //   totalPrice: '123,123,000,000원',
-      //   currentPrice: '75,399,000원',
-      //   datetime: '2021/04/03 08:23:22',
-      // },
-    ],
-  };
-
   componentDidMount() {
     // axios
     //   .get('http://localhost:4000/transactions')
     //   .then((response) => this.setState({ transactions: response.data }));
 
-    Api.get('/transactions', { params: { code: 'BTC' } }).then((response) =>
-      this.setState({ transactions: response.data }),
-    );
+    // Api.get('/transactions', { params: { code: 'BTC' } }).then((response) =>
+    //   this.setState({ transactions: response.data }),
+    // );
 
-    /*
-    9-3 참조 코드
-    Api.get('/transactions')
-      .then(({ data }) => this.props.setTransactionList(data));
-    */
+    Api.get('/transactions').then(({ data }) => this.props.setTransactionList(data));
+
     /*
     10-2 참조 코드
     this.props.requestTransactionList();
@@ -45,29 +30,24 @@ class TransactionList extends PureComponent {
   }
 
   render() {
-    const { transactions } = this.state;
-
-    // const { transactions, loading } = this.props;
+    const { transactions, loading } = this.props;
     return (
       <div>
         <Heading level={3}>거래 현황</Heading>
         <Card vertical={4} horizontal={4}>
-          {/* <TransactionSearchFilterContainer /> */}
           <TransactionSearchFilter />
         </Card>
         <Card>
-          {/* <TransactionTable transactions={transactions} isLoading={loading} /> */}
           <TransactionTable transactions={transactions} />
         </Card>
-        {/* <TransactionPaginationContainer /> */}
       </div>
     );
   }
 }
 
-// TransactionList.defaultProps = {
-//   transactions: [],
-//   requestTransactionList: () => {},
-// };
+TransactionList.defaultProps = {
+  transactions: [],
+  requestTransactionList: () => {},
+};
 
 export default TransactionList;
